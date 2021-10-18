efd4387cc7e5cde0a921ca5b2f4a1966b8be2afe
lindstrom91 <89763098+lindstrom91@users.noreply.github.com>
2021-10-18 14:42:46 -0700

    Add files via upload

--- shibscripts.js
+++ shibscripts.js
@@ -4,3 +4,9 @@ function randomCrypto() {
             window.open(randCrypto, '_blank');
 
 }    
+
+function randomYouTube() {
+    const youtubeArr = ["<iframe width="275" height="150" src="https://www.youtube.com/embed/SVFCJ-xL8YA" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay=1; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>",];
+        const randVideo = youtubeArr[Math.floor(Math.random()*youtubeArr.length)];
+        document.write(randVideo);
+}
