7495940b04a95e8a64c248cc600febbe0c96d523
lindstrom91 <89763098+lindstrom91@users.noreply.github.com>
2021-10-18 08:29:07 -0700

    Add files via upload
    randomCrypto button now working.

--- shibscripts.js
+++ shibscripts.js
@@ -1,7 +1,6 @@
 function randomCrypto() {
-   const cryptoArr = ["https://coinmarketcap.com/currencies/dogelon/", "https://coinmarketcap.com/currencies/baby-doge-coin/"];
-    randCrypto = cryptoArr[Math.floor(Math.random()*cryptoArr.length)];
-        window.open = randCrypto;
-
+   const cryptoArr = [href="https://coinmarketcap.com/currencies/dogelon/", href="https://coinmarketcap.com/currencies/baby-doge-coin/", href="https://coinmarketcap.com/currencies/rendoge/", href="https://coinmarketcap.com/currencies/dogefi/", href="https://coinmarketcap.com/currencies/bone-shibaswap/", href="https://coinmarketcap.com/currencies/shiba-puppy/", href="https://coinmarketcap.com/currencies/shibx/", href="https://coinmarketcap.com/currencies/baby-shiba-inu/"];
+    const randCrypto = cryptoArr[Math.floor(Math.random()*cryptoArr.length)];
+            window.open(randCrypto, '_blank');
 
 }    
