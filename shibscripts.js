601b444ae61ab5839ee891d62b04ffa9d99c114f
lindstrom91 <89763098+lindstrom91@users.noreply.github.com>
2021-10-17 19:52:54 -0700

    Add files via upload

---
+++ shibscripts.js
@@ -0,0 +1,7 @@
+function randomCrypto() {
+   const cryptoArr = ["https://coinmarketcap.com/currencies/dogelon/", "https://coinmarketcap.com/currencies/baby-doge-coin/"];
+    randCrypto = cryptoArr[Math.floor(Math.random()*cryptoArr.length)];
+        window.open = randCrypto;
+
+
+}    
